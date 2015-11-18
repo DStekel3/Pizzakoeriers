7f54dd4c9232d4f608867a87f51f72aa5c969913
Pattie09 <p.d.sonsma@students.uu.nl>
2015-11-18 16:10:13 +0100

    added Solution class

---
+++ Pizza/Pizza/SA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    class SA
+    {
+        int temp, a;
+        public SA(int temperature, int coolratio) // (graph, cool ratio, temperature)
+        {
+            temp = temperature;
+            a = coolratio;
+        }
+    }
+}
