06e6eb17a62a91cb31b1a03f5784927d991e91a6
DStekel3 <danielstekelenburg@hotmail.com>
2015-11-18 12:08:07 +0100

    ...

--- Pizza/Pizza/Customer.cs
+++ Pizza/Pizza/Customer.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Pizza
 {
     class Customer
     {
+        public int X { get; }
+        public int Y { get; }
+
+        public Customer(int x, int y)
+        {
+            X = x; Y = y;
+        }
     }
 }
--- Pizza/Pizza/Input.cs
+++ Pizza/Pizza/Input.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Pizza
 {
-    class Input
+    public class Input
     {
+        Customer[] customers;
+        // generates a (random) set of customers used as input
+        public Input(int amount)
+        {
+            customers = new Customer[amount];
+            for(int t =0;t< amount;t++)
+            {
+                Random r = new Random();
+                int x = r.Next(-100, 100);
+                int y = r.Next(-100, 100);
+                customers[t] = new Customer(x, y);
+            }
+        }
     }
 }
