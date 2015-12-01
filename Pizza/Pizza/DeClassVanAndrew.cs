d350c90d5569307c1aba1aa5849fa3898f559ee7
Synchrostar <synchrostar95@gmail.com>
2015-12-01 16:04:19 +0100

    Local search

--- Pizza/Pizza/DeClassVanAndrew.cs
+++ Pizza/Pizza/DeClassVanAndrew.cs
@@ -10,35 +10,27 @@ namespace Pizza
     {
         int iterations = 10; //Amount of neighbouring solutions to look at
         int cost_opt, cost_cur;
-        int[][] path;
-        Customer[] customers;
         Solution s;
-        /*
-        public DeClassVanAndrew(Customer[] cust, int[][] input)
-        {
-            path = input;
-            customers = cust;
-        }
         
-        public void CalculateInitial()
+        public DeClassVanAndrew(Solution initial_solution)
         {
-            s = new Solution(customers, path);
-            cost_opt = s.costs();
+            s = initial_solution;
+            cost_opt = s.costs(); //Initial optimal cost
         }
 
-        public void LocalSearch()
+        public Solution LocalSearch()
         {
             for(int x = 0; x < iterations; x++)
             {
-                //s.GenerateNeighbors(iterations);
-                cost_cur = s.costs();
-                if (cost_cur < cost_opt)
+                Solution s_next = s.NextNeighbor(0); //Neighboring state by swapping two customers in route
+                cost_cur = s_next.costs(); //Calculate cost of neighboring state
+                if (cost_cur < cost_opt) //If cost of neighboring state is less than current optimal...
                 {
-                    cost_opt = cost_cur;
-                    //path = s.rs;
+                    cost_opt = cost_cur; //...update optimal cost...
+                    s = s_next; //...and update best route
                 }
             }
-        }
-        */
+            return s;
+        }      
     }
 }
