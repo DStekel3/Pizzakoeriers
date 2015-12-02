72b6e85529793dbccc8fa2b00876146630bc2f7b
DStekel3 <danielstekelenburg@hotmail.com>
2015-12-02 11:44:15 +0100

    commit

--- Pizza/Pizza/Display.cs
+++ Pizza/Pizza/Display.cs
@@ -10,66 +10,73 @@ using System.Windows.Forms;
 
 namespace Pizza
 {
-    public partial class Display : Form
+  public partial class Display : Form
+  {
+    Solution sol;
+    Point depot;
+    List<Brush> brushes = new List<Brush>();
+
+    public Display(Solution solution)
     {
-        Solution sol;
-        Point depot;
-        List<Brush> brushes = new List<Brush>();
+      InitializeComponent();
+      sol = solution;
+      this.Text = "Solution Graph (costs: " + sol.costs() + ")";
+      this.Size = new Size(800, 800);
 
-        public Display(Solution solution)
-        {
-            InitializeComponent();
-            sol = solution;
-            this.Text = "Solution Graph (costs: " + sol.costs() + ")";
-            this.Size = new Size(800, 800);
+      // depot point
+      depot = new Point(this.Width / 2, this.Height / 2);
 
-            // depot point
-            depot = new Point(this.Width / 2, this.Height / 2);
+      // different strokes for different folks
+      defineBrushes();
 
-            // different strokes for different folks
-            definteBrushes();
-
-            // paint event
-            this.Paint += new PaintEventHandler(drawSolution);
-        }
-
-        public void definteBrushes()
-        {
-            brushes.Add(Brushes.Red);
-            brushes.Add(Brushes.Blue);
-            brushes.Add(Brushes.Green);
-            brushes.Add(Brushes.Yellow);
-            brushes.Add(Brushes.Violet);
-            brushes.Add(Brushes.Brown);
-            brushes.Add(Brushes.Pink);
-            brushes.Add(Brushes.Azure);
-            brushes.Add(Brushes.DarkBlue);
-            brushes.Add(Brushes.White);
-        }
-
-        public void drawSolution(object sender, PaintEventArgs pea)
-        {
-            Pen pen = new Pen(Brushes.Red);
-            foreach (Deliveryman d in sol.rs)
-            {
-                Brush brush = brushes[0];
-                brushes.RemoveAt(0);
-                Point p1 = depot;
-                
-                foreach (int p in d.route)
-                {
-                    // obtain customer location
-                    Point p2 = new Point(sol.cs[p - 1].X / 3 + this.Width / 2, sol.cs[p - 1].Y / 3 + this.Height / 2);
-                    // mark location            
-                    pea.Graphics.FillEllipse(brush, new Rectangle(p2, new Size(new Point(10, 10))));
-                    // draw route to customer
-                    pea.Graphics.DrawLine(new Pen(brush), new Point(p1.X + 5, p1.Y + 5), new Point(p2.X + 5, p2.Y + 5));
-                    // update p1
-                    p1 = p2;
-                }
-
-                pea.Graphics.FillEllipse(Brushes.Black, new Rectangle(depot, new Size(new Point(10, 10))));
-            }                     
-        }
+      // paint event
+      this.Paint += new PaintEventHandler(drawSolution);
     }
+
+    public void defineBrushes()
+    {
+      brushes.Add(Brushes.Red);
+      brushes.Add(Brushes.Blue);
+      brushes.Add(Brushes.Green);
+      brushes.Add(Brushes.Yellow);
+      brushes.Add(Brushes.Violet);
+      brushes.Add(Brushes.Brown);
+      brushes.Add(Brushes.Pink);
+      brushes.Add(Brushes.Azure);
+      brushes.Add(Brushes.DarkBlue);
+      brushes.Add(Brushes.White);
+    }
+
+    public void drawSolution(object sender, PaintEventArgs pea)
+    {
+      defineBrushes();
+      Pen pen = new Pen(Brushes.Red);
+      foreach (Deliveryman d in sol.rs)
+      {
+        Brush brush = null;
+        if (brushes.Count != 0)
+        {
+           brush = brushes[0];
+          brushes.RemoveAt(0);
+        }
+        else
+          brush = Brushes.Red;
+        Point p1 = depot;
+
+        foreach (int p in d.route)
+        {
+          // obtain customer location
+          Point p2 = new Point(sol.cs[p - 1].X / 3 + this.Width / 2, sol.cs[p - 1].Y / 3 + this.Height / 2);
+          // mark location            
+          pea.Graphics.FillEllipse(brush, new Rectangle(p2, new Size(new Point(10, 10))));
+          // draw route to customer
+          pea.Graphics.DrawLine(new Pen(brush), new Point(p1.X + 5, p1.Y + 5), new Point(p2.X + 5, p2.Y + 5));
+          // update p1
+          p1 = p2;
+        }
+
+        pea.Graphics.FillEllipse(Brushes.Black, new Rectangle(depot, new Size(new Point(10, 10))));
+      }
+    }
+  }
 }
--- Pizza/Pizza/Solution.cs
+++ Pizza/Pizza/Solution.cs
@@ -7,155 +7,126 @@ using System.Windows.Forms;
 
 namespace Pizza
 {
-    public class Solution
+  public class Solution
+  {
+    int n;
+    public Customer[] cs;
+    public Deliveryman[] rs;
+    Random rnd;
+
+    public Solution(Customer[] customers, Deliveryman[] d)
     {
-        int n;
-        public Customer[] cs;
-        public Deliveryman[] rs;
-        Random rnd;
-        
-        public Solution(Customer[] customers, Deliveryman[] d)
-        {
-            // customers, or 'map' of the problem            
-            cs = customers;
-            // representation of the routes, by customer id's
-            rs = d;            
-            // number of deliverymen
-            n = d.Length;
-            // rnd
-            rnd = new Random();
-        }
+      // customers, or 'map' of the problem            
+      cs = customers;
+      // representation of the routes, by customer id's
+      rs = d;
+      // number of deliverymen
+      n = d.Length;
+      // rnd
+      rnd = new Random();
+    }
 
-        public int Heuristic(int h)
-        {
-            // Heuristic value
-            int heuristic = 0;
+    public int Heuristic(int h)
+    {
+      // Heuristic value
+      int heuristic = 0;
 
-            // --- Heuristics ---
-            // Heuristic 1:
-            if (h == 0)
-            {
-                heuristic = costs();
-                /*
-                for (int i = 0; i < n; i++)
-                {
-                    // number of customers on the route
-                    int nodes = rs[i].Length;
-                    // total distance of the route
-                    int dist = 0;
-                    int jump = nodes / 4;
-
-                    for (int node = 0; node < nodes; node += jump)
-                    {
-
-                    }
-                }
-                */
-            }
-            
-            return heuristic;            
-        }
-
-        public int costs()
-        {
-            // returns the actual costs of the solution
-            int costs = 0;
-            for (int i = 0; i < n; i++)
-            {
-                // number of customers on the route
-                int nodes = rs[i].route.Count;
-                // total distance of the route
-                int dist = 0;
-                // customer id's and pos
-                int id_a = 0;
-                int x_a = 0;
-                int y_a = 0;
-                int id_b, x_b, y_b;
-
-                for (int j = 0; j < nodes; j++)
-                {
-                    // get node id
-                    id_b = rs[i].route[j];
-                    // get node position
-                    x_b = cs[id_b - 1].X;
-                    y_b = cs[id_b - 1].Y;
-                    // calculate distance between node a and b
-                    dist += Math.Abs(x_a - x_b) + Math.Abs(y_a - y_b);
-                    // a = b
-                    id_a = id_b;
-                    x_a = x_b;
-                    y_a = y_b;
-                }
-
-                // add route distance to total heuristic value
-                costs += dist;
-            }
-            return costs;
-        }
-
+      // --- Heuristics ---
+      // Heuristic 1:
+      if (h == 0)
+      {
+        heuristic = costs();
         /*
-        public List<Solution> GenerateNeighbors(int g, int num)
+        for (int i = 0; i < n; i++)
         {
-            List<Solution> gs = new List<Solution>();
+            // number of customers on the route
+            int nodes = rs[i].Length;
+            // total distance of the route
+            int dist = 0;
+            int jump = nodes / 4;
 
-            // Neighbor Gen I
-            if (g == 0)
+            for (int node = 0; node < nodes; node += jump)
             {
-                // generate n neighbors
-                for (int i = 0; i < num; i++)
-                {
-                    Deliveryman[] rx = rs;
 
-                    // Rnd route and nodes
-                    int rnd_route = rnd.Next(0, n);
-                    int rnd_nodeA = rnd.Next(0, rx[rnd_route].route.Count);
-                    int rnd_nodeB = rnd.Next(0, rx[rnd_route].route.Count);
-
-                    // Swap nodes
-                    int a = rs[rnd_route].route[rnd_nodeA];
-                    rx[rnd_route].route[rnd_nodeA] = rx[rnd_route].route[rnd_nodeB];
-                    rx[rnd_route].route[rnd_nodeB] = a;
-
-                    gs.Add(new Solution(cs, rx));
-                }
             }
-            return gs;
         }
         */
+      }
 
-        public Solution NextNeighbor(int g)
-        {
-            if (g == 0)
-            {
-                Deliveryman[] rx = new Deliveryman[rs.Length];
-                for (int i = 0; i < rs.Length; i++)
-                {
-                    rx[i] = new Deliveryman();
-                    foreach (int c in rs[i].route)
-                        rx[i].route.Add(c);
-                }
-
-                // Rnd route and nodes
-                int rnd_route = rnd.Next(0, n);
-                int rnd_nodeA = rnd.Next(0, rx[rnd_route].route.Count);
-                int rnd_nodeB = rnd.Next(0, rx[rnd_route].route.Count);
-
-                // Swap nodes
-                int a = rx[rnd_route].route[rnd_nodeA];
-                rx[rnd_route].route[rnd_nodeA] = rx[rnd_route].route[rnd_nodeB];
-                rx[rnd_route].route[rnd_nodeB] = a;
-
-                // Return new Solution
-                return new Solution(cs, rx);
-            }
-            else
-                return new Solution(cs, rs);
-        }
-
-        public void Draw()
-        {
-            Display display = new Display(this);
-            Application.Run(display);
-        }
+      return heuristic;
     }
+
+    public int costs()
+    {
+      // returns the actual costs of the solution
+      int costs = 0;
+      for (int i = 0; i < n; i++)
+      {
+        // number of customers on the route
+        int nodes = rs[i].route.Count;
+        // total distance of the route
+        int dist = 0;
+        // customer id's and pos
+        int id_a = 0;
+        int x_a = 0;
+        int y_a = 0;
+        int id_b, x_b, y_b;
+
+        for (int j = 0; j < nodes; j++)
+        {
+          // get node id
+          id_b = rs[i].route[j];
+          // get node position
+          x_b = cs[id_b - 1].X;
+          y_b = cs[id_b - 1].Y;
+          // calculate distance between node a and b
+          dist += Math.Abs(x_a - x_b) + Math.Abs(y_a - y_b);
+          // a = b
+          id_a = id_b;
+          x_a = x_b;
+          y_a = y_b;
+        }
+
+        // add route distance to total heuristic value
+        costs += dist;
+      }
+      return costs;
+    }
+
+    public Solution NextNeighbor(int g)
+    {
+      //Choose a random customer. Replace this customer within own route.
+      if (g == 0)
+      {
+        Deliveryman[] rx = new Deliveryman[rs.Length];
+        for (int i = 0; i < rs.Length; i++)
+        {
+          rx[i] = new Deliveryman();
+          foreach (int c in rs[i].route)
+            rx[i].route.Add(c);
+        }
+
+        // Rnd route and nodes
+        int rnd_route = rnd.Next(0, n);
+        int rnd_nodeA = rnd.Next(0, rx[rnd_route].route.Count);
+        int rnd_nodeB = rnd.Next(0, rx[rnd_route].route.Count);
+
+        // Swap nodes
+        int a = rx[rnd_route].route[rnd_nodeA];
+        rx[rnd_route].route[rnd_nodeA] = rx[rnd_route].route[rnd_nodeB];
+        rx[rnd_route].route[rnd_nodeB] = a;
+
+        // Return new Solution
+        return new Solution(cs, rx);
+      }
+      else
+        return new Solution(cs, rs);
+    }
+
+    public void Draw()
+    {
+      Display display = new Display(this);
+      Application.Run(display);
+    }
+  }
 }
