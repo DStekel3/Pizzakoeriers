b308986c836b78fc6da146b113a0301b7ce9c89d
Pattie09 <p.d.sonsma@students.uu.nl>
2015-12-07 11:58:31 +0100

    commit

--- Pizza/Pizza/Console.cs
+++ Pizza/Pizza/Console.cs
@@ -15,7 +15,7 @@ namespace Pizza
             Solution initial_solution = init.getSolution(1);
             while (true)
             {                
-                SimulatedAnnealing sa = new SimulatedAnnealing(initial_solution, 100, 0.95f, 0, 2);
+                SimulatedAnnealing sa = new SimulatedAnnealing(initial_solution, 10, 0.95f, 0, 2);
                 Solution result_solution = sa.run();
 
                 // Present results
